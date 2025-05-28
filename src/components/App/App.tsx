f76c020f95760a9176d47faf575a76bd2c7a9684
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-05-28 10:34:07 +0200

    Edited movieServices types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import css from "./App.module.css"
 import SearchBar from '../SearchBar/SearchBar'
 import { Toaster } from "react-hot-toast"
 import type { Movie } from "../../types/movie"
-import { fetchMovies } from "../../services/movieServices"
+import { fetchMovies } from "../../services/movieService"
 import { useState } from "react"
 import toast from "react-hot-toast"
 import MovieGrid from "../MovieGrid/MovieGrid"
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
+interface Response {
+    results: Movie[];
+}
 
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN as string;
 const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
@@ -8,7 +11,7 @@ const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&langu
 
 export async function fetchMovies(query:string):Promise<Movie[]> {
     try {
-        const response = await axios.get(url, {
+        const response = await axios.get<Response>(url, {
             method: 'GET',
             headers: {
                 Accept: 'application/json',
